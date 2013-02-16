bd921a980c31872084c3959cb036c03d735ef08a
nroxana <necula.roxana91@gmail.com>
2013-02-16 22:30:27 +0200

    Changes to be committed: 	deleted:    README 	deleted:    script2p.sh 	deleted:    script2s.sh

--- 1APD/script2p.sh
+++
@@ -1,7 +0,0 @@
-        T=$1
-        FISIN=$2
-        FISOUT=$3
-
-        export OMP_SCHEDULE="dynamic"
-        export OMP_NUM_THREADS=4
-        time ./paralel $T $FISIN $FISOUT
--- 1APD/script2s.sh
+++
@@ -1,5 +0,0 @@
-		T=$1
-        FISIN=$2
-        FISOUT=$3
-
-        time ./serial $T $FISIN $FISOUT
